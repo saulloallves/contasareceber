edd99761b5c7a24572bda9e92f9888b67b98d3d5
saulloallves <conciergevirtual@crescieperdi.com>
2025-07-25 10:26:20 -0300

    Updated DashboardGeral.tsx

--- src/components/Dashboard/DashboardGeral.tsx
+++ src/components/Dashboard/DashboardGeral.tsx
@@ -51,7 +51,7 @@ export function DashboardGeral() {
   }
 
   return (
-    <div className="space-y-6 p-5">
+    <div className="space-y-6 p-6">
       {/* Header */}
       <div className="flex items-center justify-between">
         <div>
